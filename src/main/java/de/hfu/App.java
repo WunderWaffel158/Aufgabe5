2acd82e4086e2df60043f542328bc2f001c2577b
Reser <artur.reser@hs-furtwangen.de>
2022-12-01 08:49:42 +0000

    comments added 2.0

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,11 +1,17 @@
 package de.hfu;
 
-import java.io.BufferedReader;
+/*
+ * This is a very important code that I'll sell to Microsoft for a billion at least
+ */
+
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 
 public class App {
 	public static void main(String args[]) throws IOException{
 		String input = System.console().readLine();//new BufferedReader(new InputStreamReader(System.in)).readLine();
-		System.out.println(input.toUpperCase());}
+		System.out.println(input.toUpperCase());
+	return;	
+	}
 	}
